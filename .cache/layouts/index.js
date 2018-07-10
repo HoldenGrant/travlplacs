08102d0c1e845a8c6e26429df3877d0c58dba9bd
HoldenGrant <holdengrantcausing@gmail.com>
2018-07-10 19:05:07 +0800

    .cache folder

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/Users/Holden Grant/Documents/webApp/travlplacs/src/layouts/index.js"
+  import data from "C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ .cache/sync-requires.js
@@ -0,0 +1,23 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.layouts = {
+  "layout---index": preferDefault(require("C:/Users/Holden Grant/Documents/webApp/travlplacs/.cache/layouts/index.js"))
+}
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\src\\pages\\page-2.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\layout-index.json"),
+  "dev-404-page.json": require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\dev-404-page.json"),
+  "404.json": require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\404.json"),
+  "index.json": require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\index.json"),
+  "page-2.json": require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\page-2.json"),
+  "404-html.json": require("C:\\Users\\Holden Grant\\Documents\\webApp\\travlplacs\\.cache\\json\\404-html.json")
+}
