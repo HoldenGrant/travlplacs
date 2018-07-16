d03f8731220612ac2f8203a6976a746fbb48937b
HoldenGrant <holdengrantcausing@gmail.com>
2018-07-17 00:16:09 +0800

    nav collapse on mobile

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,15 +2,28 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 class Nav extends React.Component{
+    constructor(){
+        super()
+        this.state= {
+            collapse: true,
+        };
+    }
+
+    toggleCollapse(){
+        const collapse = !this.state.collapse;
+        this.setState({collapse});
+    }
     render(){
+        const {collapse } = this.state;
+        const navClass = collapse ? "collapse" : "";
         return(
             <nav className='navbar navbar-expand-lg navbar-light bg-light'>
                 <div className='container'>
-                    <button className='navbar-toggler ml-auto' type='button'>
+                    <button className='navbar-toggler ml-auto' type='button' onClick={this.toggleCollapse.bind(this)}>
                         <span className='navbar-toggler-icon'></span>
                     </button>
 
-                    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                    <div className={'navbar-collapse ' + navClass} id='navbarSupportedContent'>
                         <ul className='navbar-nav ml-auto'>
                             <li className='nav-item'>
                                 <Link className='nav-link' activeClassName="active" exact to="/">Home</Link>
